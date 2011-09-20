be161d160c0423eed98ffb81cc8722d37619c15c
Tobias Pfeiffer <tobias.pfeiffer@student.hpi.uni-potsdam.de>
2011-09-20 13:12:22 +0200

    Some specs definitely help (they helped me, can't beleive how many bugs I found - should revert to being more TDD)

--- lib/node.rb
+++ lib/node.rb
@@ -8,14 +8,15 @@ class Node
   attr_reader :rumors
   attr_accessor :adjacent_nodes
 
-  def initialize(name, adjacent_nodes)
-    self.name = name
-    adjacent_nodes.each { |nod| add_adjacent_node(node) }
+  def initialize(name, adjacent_nodes=[])
+    @name = name
+    @adjacent_nodes = []
+    adjacent_nodes.each { |node| add_adjacent_node(node) }
     @rumors = {}
   end
 
   def add_adjacent_node(node)
-    @adjacent_nodes << node
+    adjacent_nodes << node
     node.adjacent_nodes << self
   end
 
---
+++ spec/node_spec.rb
@@ -0,0 +1,41 @@
+require_relative '../lib/node'
+
+describe "Node" do
+
+  before :each do
+    @node = Node.new("Hello Node")
+  end
+
+  it "can be created with a name" do
+    @node.should_not be nil
+  end
+
+  it "should not have adjacent nodes when created with just a name" do
+    @node.adjacent_nodes.empty?.should == true
+  end
+
+  it "may have other nodes added" do
+    other_node = Node.new("I am something else")
+    @node.add_adjacent_node other_node
+
+    @node.adjacent_nodes.empty?.should == false
+    @node.adjacent_nodes.first.should eq other_node
+  end
+
+  it "should add itself to the adjacent nodes list of the adjacent node" do
+    other_node = Node.new("I got no neighbours :-(")
+    @node.add_adjacent_node(other_node)
+
+    other_node.adjacent_nodes.empty?.should == false
+    other_node.adjacent_nodes.first.should eq @node
+  end
+
+  it "may be created with an adjacent node given" do
+    other_node = Node.new("Look I got neighbours! :-)", [@node])
+
+    other_node.adjacent_nodes.empty?.should == false
+    other_node.adjacent_nodes.first.should eq @node
+  end
+
+end
+
