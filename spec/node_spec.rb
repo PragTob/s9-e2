d01b8e69a4a6aac7108d43c01c37004dabcd2583
Tobias Pfeiffer <tobias.pfeiffer@student.hpi.uni-potsdam.de>
2011-09-20 14:16:07 +0200

    node_has_adjacent_node refactoring

--- spec/node_spec.rb
+++ spec/node_spec.rb
@@ -1,5 +1,10 @@
 require_relative '../lib/node'
 
+def node_has_adjacent_node(node, adjacent_node)
+  node.adjacent_nodes.empty?.should == false
+  node.adjacent_nodes.first.should eq adjacent_node
+end
+
 describe "Node" do
 
   before :each do
@@ -18,23 +23,20 @@ describe "Node" do
     other_node = Node.new("I am something else")
     @node.add_adjacent_node other_node
 
-    @node.adjacent_nodes.empty?.should == false
-    @node.adjacent_nodes.first.should eq other_node
+    node_has_adjacent_node(@node, other_node)
   end
 
   it "should add itself to the adjacent nodes list of the adjacent node" do
     other_node = Node.new("I got no neighbours :-(")
     @node.add_adjacent_node(other_node)
 
-    other_node.adjacent_nodes.empty?.should == false
-    other_node.adjacent_nodes.first.should eq @node
+    node_has_adjacent_node(other_node, @node)
   end
 
   it "may be created with an adjacent node given" do
     other_node = Node.new("Look I got neighbours! :-)", [@node])
 
-    other_node.adjacent_nodes.empty?.should == false
-    other_node.adjacent_nodes.first.should eq @node
+    node_has_adjacent_node(other_node, @node)
   end
 
   it "may be created with multiple adjacent nodes given" do
@@ -43,6 +45,10 @@ describe "Node" do
     friendly_node = Node.new("I got lots of friends!", [@node, mini_node])
     friendly_node.adjacent_nodes.should == [@node, mini_node]
   end
+
+#  it "adds itself as an adjacent to nodes given in the constructor" do
+#    other_node = Node.new("Hello", [@node])
+
 
 end
 
