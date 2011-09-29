013830f67831660b657d5cc304b60c729c83f9b0
Tobias Pfeiffer <tobias.pfeiffer@student.hpi.uni-potsdam.de>
2011-09-29 10:53:01 +0200

    A little rearranging for the node class.

--- lib/siwoti/node.rb
+++ lib/siwoti/node.rb
@@ -1,15 +1,13 @@
 module Siwoti
   class Node
 
-    attr_reader :name
-
-    # rumors is a hash pointing from a rumor to the % this node believes in it
-    attr_reader :rumors
+    attr_reader :name, :rumors
 
     # we have the graph so we can check for our adjacent nodes
     def initialize(name, graph)
       @name = name
       @graph = graph
+      # rumors is a hash pointing from a rumor to the % this node believes in it
       @rumors = {}
     end
 
