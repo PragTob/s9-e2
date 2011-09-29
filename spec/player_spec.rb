40db21f32b6329032c7fac7ea9a1560d2869d36d
Tobias Pfeiffer <tobias.pfeiffer@student.hpi.uni-potsdam.de>
2011-09-29 19:34:49 +0200

    player spec updated

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -2,10 +2,30 @@ require_relative 'spec_helper'
 
 describe "Player" do
 
-  it "should be created with a name" do
-    player = Player.new "Tobi"
-    player.name.should == "Tobi"
+  before :each do
+    @player = Player.new("Tobi")
   end
+
+  it "should be created with a name" do
+    @player.name.should == "Tobi"
+  end
+
+  it "should have an amount of hours after creation" do
+    @player.hours.should > 0
+  end
+
+  it "should have time left when created" do
+    @player.out_of_time?.should be false
+  end
+
+  it "should reset the hours when a new turn is initiated" do
+    @player.hours = 0
+
+    @player.new_turn
+
+    @player.hours.should > 0
+  end
+
 
 end
 
